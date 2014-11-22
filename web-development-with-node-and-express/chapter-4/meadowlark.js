b154188cf9b73927c016fa9cb6dcab72a647bfb7
mserino <m.serino@hotmail.it>
2014-11-22 22:11:31 +0100

    Added functional tests to single tour pages and unit test to fortune cookie

--- web-development-with-node-and-express/chapter-4/meadowlark.js
+++ web-development-with-node-and-express/chapter-4/meadowlark.js
@@ -13,12 +13,34 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
 
+// testing
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	next();
+});
+
+// routes
 app.get('/', function(req, res){
 	res.render('home');
 });
 
 app.get('/about', function(req, res){
-	res.render('about', { fortune: fortune.getFortune() });
+	res.render('about', { 
+			fortune: fortune.getFortune(),
+			pageTestScript: '/qa/tests-about.js'
+		});
+});
+
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+
+app.get('/tours/oregon-coast', function(req, res){
+	res.render('tours/oregon-coast');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
 });
 
 // custom 404 page
