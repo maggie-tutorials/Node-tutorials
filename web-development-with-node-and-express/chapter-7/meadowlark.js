1369cb3443e7e9676ef64dc2851246d71180aba8
mserino <m.serino@hotmail.it>
2014-11-24 23:27:00 +0100

    Node and Express - uploading files

--- web-development-with-node-and-express/chapter-7/meadowlark.js
+++ web-development-with-node-and-express/chapter-7/meadowlark.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
+var formidable = require('formidable');
 
 var app = express();
 
@@ -79,6 +80,25 @@ app.post('/process', function(req, res){
 
 app.get('/thank-you', function(req,res){
 	res.render('thank-you');
+});
+
+app.get('/contest/vacation-photo', function(req, res){
+	var now = new Date();
+	res.render('contest/vacation-photo', {
+		year: now.getFullYear(), month: now.getMonth()
+	});
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files){
+		if(err) return res.redirect(303, '/error');
+		console.log('received fields:');
+		console.log(fields);
+		console.log('received files');
+		console.log(files);
+		res.redirect(303, '/thank-you');
+	});
 });
 
 // custom 404 page
