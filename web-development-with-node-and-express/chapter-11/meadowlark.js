d5304e8b58e17a4a09380345f381ce27c485f259
mserino <m.serino@hotmail.it>
2014-11-26 18:56:56 +0100

    Middleware examples in chapter 10 and sending emails in chapter 11

---
+++ web-development-with-node-and-express/chapter-11/meadowlark.js
@@ -0,0 +1,175 @@
+var express = require('express');
+var fortune = require('./lib/fortune.js');
+var formidable = require('formidable');
+var credentials = require('./credentials.js');
+var nodemailer = require('nodemailer');
+
+var app = express();
+
+// set up handlebars view engine
+var handlebars = require('express3-handlebars')
+						.create({
+							defaultLayout: 'main',
+							helpers: {
+								section: function(name, options){
+									if(!this._sections) this._sections = {};
+									this._sections[name] = options.fn(this);
+									return null;
+								}
+							}
+						});
+
+app.engine('handlebars', handlebars.engine);
+
+// set up nodemailer
+var mailTransport = nodemailer.createTransport('SMTP', {
+	service: 'Gmail',
+	auth: {
+		user: credentials.gmail.user,
+		pass: credentials.gmail.password
+	}
+});
+
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+
+// middlewares
+app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser')());
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')());
+app.use(function(req, res, next){
+	res.locals.flash = req.session.flash;
+	delete req.session.flash;
+	next();
+});
+
+// testing
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	next();
+});
+
+// weather partial
+app.use(function(req, res, next){
+	if(!res.locals.partials) res.locals.partials = {};
+	res.locals.partials.weather = getWeatherData();
+	next();
+});
+
+// routes
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('/about', function(req, res){
+	mailTransport.sendMail({
+		from: '"Meadowlark Travel" <info@meadowlarktravel.com>',
+		to: 'manicatore@gmail.com',
+		subject: 'Your Meadowlark Travel Tour',
+		text: 'Thank you for booking your trip with Meadowlark Travel. We look forward to your visit'
+	}, function(err){
+		if(err) console.error('Unable to send email: ' + err);
+	});
+
+	res.render('about', { 
+			fortune: fortune.getFortune(),
+			pageTestScript: '/qa/tests-about.js'
+		});
+});
+
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+
+app.get('/tours/oregon-coast', function(req, res){
+	res.render('tours/oregon-coast');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
+});
+
+app.get('/newsletter', function(req, res){
+	res.render('newsletter', {csrf: 'CSRF token goes here'});
+});
+
+app.get('/jquery-test', function(req, res){
+	res.render('jquery-test');
+});
+
+app.post('/process', function(req, res){
+	if(req.xhr || req.accepts('json,html')==='json'){
+		res.send({ success: true });
+	} else {
+		res.redirect(303, '/thank-you');
+	}
+});
+
+app.get('/thank-you', function(req,res){
+	res.render('thank-you');
+});
+
+app.get('/contest/vacation-photo', function(req, res){
+	var now = new Date();
+	res.render('contest/vacation-photo', {
+		year: now.getFullYear(), month: now.getMonth()
+	});
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files){
+		if(err) return res.redirect(303, '/error');
+		console.log('received fields:');
+		console.log(fields);
+		console.log('received files');
+		console.log(files);
+		res.redirect(303, '/thank-you');
+	});
+});
+
+// custom 404 page
+app.use(function(req, res){
+	res.status(404);
+	res.render('404');
+});
+
+// custom 500 page
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
+
+function getWeatherData(){
+	return {
+		locations: [
+			{
+				name: 'Portland',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+				weather: 'Overcast',
+				temp: '54.1 F (12.3 C)',
+			},
+			{
+				name: 'Bend',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+				weather: 'Partly Cloudy',
+				temp: '55.0 F (12.8 C)',
+			},
+			{
+				name: 'Manznita',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+				weather: 'Light Rain',
+				temp: '55.0 F (12.8 C)',
+			},
+		],
+	};
+}
